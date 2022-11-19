bcb1abdde358c4ea509f714a9476203c4d1011db
Matthew Meredith <matthewmeredith775@gmail.com>
2022-11-19 15:12:16 -0500

    Globalify static items, implement PaginatedMenu

--- src/main/kotlin/net/mcmerdith/guildedmenu/gui/EconomyMenu.kt
+++ src/main/kotlin/net/mcmerdith/guildedmenu/gui/EconomyMenu.kt
@@ -1,72 +1,58 @@
 package net.mcmerdith.guildedmenu.gui
 
+import PlayerBalanceMenu
 import net.mcmerdith.guildedmenu.gui.util.*
 import net.mcmerdith.guildedmenu.integration.IntegrationManager
 import net.mcmerdith.guildedmenu.integration.VaultIntegration
-import net.mcmerdith.guildedmenu.util.Extensions.addLore
-import net.mcmerdith.guildedmenu.util.Extensions.name
 import net.mcmerdith.guildedmenu.util.Extensions.setLore
+import net.mcmerdith.guildedmenu.util.Extensions.setName
 import net.mcmerdith.guildedmenu.util.GMLogger
-import net.mcmerdith.guildedmenu.util.Globals
 import org.ipvp.canvas.Menu
 
-class EconomyMenu(parent: Menu? = null) {
+class EconomyMenu(parent: Menu? = null) : PaginatedMenu {
     companion object {
         val LOGGER = GMLogger.getLogger("EconomyMenu")
+
+        val ERROR_ITEM = ItemTemplates.ERROR.setLore("Failed to retrieve balance")
+        val MONEY_ITEM = ItemTemplates.MONEYBLOCK.setName()
     }
 
-    class PlayerBalanceMenu(parent: Menu? = null) : BaseMenu(
-        "Player Balance",
-        MenuSize(3),
-        parent
-    )
-
-    val TEMPLATE = BaseMenu.Builder(6).title("Economy").redraw(true).parent(parent)
+    private val template: BaseMenu.Builder = BaseMenu.Builder(6).title("Economy").redraw(true).parent(parent)
 
     private val vault = IntegrationManager[VaultIntegration::class.java]!!
-    private val balances = vault.allBalances()
+    private val balTop = vault.topBalances()
 
-    private val offset = balances.lastOrNull()?.balance ?: 0.0
-    private val offsetMax = balances.firstOrNull()?.balance?.minus(offset) ?: 0.0
+    private fun getBlockCount(balance: Double) = (balTop.percentile(balance) * 8).toInt().coerceIn(0, 8)
 
-    private fun getBlockCount(balance: Double) = (((balance - offset) / offsetMax) * 8).toInt().coerceIn(0, 8)
-
-    private val pages: List<Menu> = GuiUtil.getPagination(TEMPLATE, GuiUtil.getRowMask(5, "100000000"))
+    private val pages: List<Menu> = GuiUtil.getPagination(template, GuiUtil.getRowMask(5, "100000000"))
         .apply {
-            for (balance in balances) {
-                val template = PlayerHeadItemTemplate.of(balance.player)
-                template.rawItem.setLore(vault.econ?.format(balance.balance) ?: balance.balance.toString())
-                addItem(template)
-            }
+            for (balance in balTop.balances) addItem(vault.getPlayerBalanceHeadTemplate(balance.player))
         }.build()
 
     init {
         for (menu in pages) {
             for (i in 1..5) {
-                val player = (menu.getSlot(i, 1).settings.itemTemplate as? PlayerHeadItemTemplate)?.player ?: continue
+                val slot = menu.getSlot(i, 1)
+                val target = (slot.settings.itemTemplate as? PlayerHeadItemTemplate)?.player ?: continue
 
                 try {
-                    val balance = balances.find { p -> p.player.uniqueId == player.uniqueId }
-                        ?: throw RuntimeException("Could not identify player ${player.name}")
+                    val balance = balTop.balances.find { p -> p.player.uniqueId == target.uniqueId }
+                        ?: throw RuntimeException("Could not identify player ${target.name}")
 
-                    for (j in 2..getBlockCount(balance.balance) + 1) {
-                        menu.getSlot(i, j).item = ItemTemplates.MONEYBLOCK.name()
-                    }
+                    GuiUtil.openScreenOnClick(slot, PlayerBalanceMenu(menu, balance.player))
+
+                    for (j in 2..getBlockCount(balance.balance) + 1) menu.getSlot(i, j).item = MONEY_ITEM
                 } catch (e: Exception) {
                     LOGGER.warn(e.message ?: "An unknown error occurred")
 
-                    val error = ItemTemplates.ERROR.setLore("Failed to retrieve balance")
-
-                    for (j in 2..9) {
-                        menu.getSlot(i, j).item = error
-                    }
+                    for (j in 2..9) menu.getSlot(i, j).item = ERROR_ITEM
                 }
 
             }
         }
     }
 
-    fun get(): Menu {
+    override fun get(): Menu {
         return pages.first()
     }
 }
