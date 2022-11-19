25bd96f6f51cda3dc2d4bfeaa1aa67a9678fc744
Matthew Meredith <matthewmeredith775@gmail.com>
2022-11-19 15:10:39 -0500

    Integrate AnvilGUI, Add controls for when no next/previous page exists

--- src/main/kotlin/net/mcmerdith/guildedmenu/gui/util/GuiUtil.kt
+++ src/main/kotlin/net/mcmerdith/guildedmenu/gui/util/GuiUtil.kt
@@ -1,17 +1,22 @@
 package net.mcmerdith.guildedmenu.gui.util
 
+import net.mcmerdith.guildedmenu.GuildedMenu
 import net.mcmerdith.guildedmenu.gui.MainMenu
 import net.mcmerdith.guildedmenu.util.ChatUtils.sendErrorMessage
 import net.mcmerdith.guildedmenu.util.Globals
+import net.wesjd.anvilgui.AnvilGUI
+import org.bukkit.Bukkit
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
 import org.ipvp.canvas.Menu
 import org.ipvp.canvas.mask.BinaryMask
 import org.ipvp.canvas.paginate.PaginatedMenuBuilder
 import org.ipvp.canvas.slot.Slot
 import org.ipvp.canvas.type.AbstractMenu
+import java.util.function.BiFunction
 
 object GuiUtil : CommandExecutor {
     fun getFullRowMask(rows: Int): BinaryMask = getRowMask(rows, "111111111")
@@ -27,14 +32,16 @@ object GuiUtil : CommandExecutor {
         return builder.build()
     }
 
-    fun getSlotNumber(row: Int, column: Int): Int = (row - 1) * 9 + column - 1
+    fun getSlotIndex(row: Int, column: Int): Int = (row - 1) * 9 + column - 1
 
     fun getPagination(builder: AbstractMenu.Builder<*>, mask: BinaryMask) = PaginatedMenuBuilder.builder(builder)
         .slots(mask)
         .previousButton(ItemTemplates.PREV_BUTTON)
-        .previousButtonSlot(getSlotNumber(6, 1))
+        .previousButtonEmpty(ItemTemplates.PREV_BUTTON_DISABLED)
+        .previousButtonSlot(getSlotIndex(6, 1))
         .nextButton(ItemTemplates.NEXT_BUTTON)
-        .nextButtonSlot(getSlotNumber(6, 9))
+        .nextButtonEmpty(ItemTemplates.NEXT_BUTTON_DISABLED)
+        .nextButtonSlot(getSlotIndex(6, 9))
 
     /**
      * When [slot] is clicked open the [leftClick] menu.
@@ -49,6 +56,20 @@ object GuiUtil : CommandExecutor {
             else leftClick.open(player)
         }
     }
+
+    fun getAnvilGUIBuilder(
+        title: String,
+        item: ItemStack,
+        parent: Menu? = null,
+        callback: BiFunction<Player, String, AnvilGUI.Response>
+    ): AnvilGUI.Builder = AnvilGUI.Builder()
+        .onClose {
+            Bukkit.getScheduler().runTask(GuildedMenu.plugin) { -> parent?.open(it) }
+        }
+        .onComplete(callback)
+        .itemLeft(item)
+        .title(title)
+        .plugin(GuildedMenu.plugin)
 
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
         sender as? Player ?: run {
