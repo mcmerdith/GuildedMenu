fe516b71cbf255a1b927ebd2693fddf1b23c4b8c
Matthew Meredith <matthewmeredith775@gmail.com>
2022-11-19 15:11:12 -0500

    EssentialsIntegration handles TPA

--- src/main/kotlin/net/mcmerdith/guildedmenu/integration/EssentialsIntegration.kt
+++ src/main/kotlin/net/mcmerdith/guildedmenu/integration/EssentialsIntegration.kt
@@ -1,7 +1,31 @@
 package net.mcmerdith.guildedmenu.integration
 
+import net.mcmerdith.guildedmenu.util.ChatUtils.sendErrorMessage
+import org.bukkit.OfflinePlayer
+import org.bukkit.entity.Player
+
 class EssentialsIntegration : Integration("Essentials") {
     override fun onEnable(): Boolean {
         return true
     }
+
+    private fun getTPAExecutor(tpahere: Boolean) =
+        fun(player: Player, target: OfflinePlayer) {
+            if (player.uniqueId == target.uniqueId) {
+                player.sendErrorMessage("You can't TPA to yourself!")
+                return
+            }
+
+            if (target.isOnline) {
+                player.performCommand("tpa${if (tpahere) "here" else ""} ${target.name}")
+            } else {
+                player.sendErrorMessage("Could not TPA! (is the target online?)")
+            }
+
+            player.closeInventory()
+        }
+
+    fun getTPAExecutor() = getTPAExecutor(false)
+
+    fun getTPAHereExecutor() = getTPAExecutor(true)
 }
