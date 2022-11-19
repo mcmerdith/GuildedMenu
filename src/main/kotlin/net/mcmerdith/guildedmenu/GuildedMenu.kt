79103273be52f15b4c6447543204607c6b62b039
Matthew Meredith <matthewmeredith775@gmail.com>
2022-11-19 15:08:32 -0500

    MenuConfig

--- src/main/kotlin/net/mcmerdith/guildedmenu/GuildedMenu.kt
+++ src/main/kotlin/net/mcmerdith/guildedmenu/GuildedMenu.kt
@@ -1,11 +1,12 @@
 package net.mcmerdith.guildedmenu
 
 import net.mcmerdith.guildedmenu.components.BusinessManager
+import net.mcmerdith.guildedmenu.configuration.MenuConfig
 import net.mcmerdith.guildedmenu.configuration.PluginConfig
 import net.mcmerdith.guildedmenu.gui.util.GuiUtil
-import net.mcmerdith.guildedmenu.util.Globals
 import net.mcmerdith.guildedmenu.integration.IntegrationManager
 import net.mcmerdith.guildedmenu.util.GMLogger
+import net.mcmerdith.guildedmenu.util.Globals
 import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 import org.ipvp.canvas.MenuFunctionListener
@@ -19,12 +20,13 @@ class GuildedMenu : JavaPlugin() {
         lateinit var plugin: GuildedMenu
     }
 
-    lateinit var configuration: PluginConfig
+    lateinit var config: PluginConfig
+    lateinit var menuConfig: MenuConfig
 
     override fun onLoad() {
         GMLogger.init(logger)
 
-        Globals.init(this);
+        Globals.init(this)
         IntegrationManager.setup()
         BusinessManager.init()
 
@@ -35,7 +37,8 @@ class GuildedMenu : JavaPlugin() {
         super.onEnable()
 
         // Load the configuration
-        configuration = PluginConfig.getNewConfig(dataFolder)
+        config = PluginConfig.create(dataFolder)
+        menuConfig = MenuConfig.create(dataFolder)
 
         // Set up our integrations
         IntegrationManager.enable()
